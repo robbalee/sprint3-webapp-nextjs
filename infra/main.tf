f31af314225c9de0e4f117011c77fbef5ff899d8
robbale <robbale41@gmail.com>
2025-02-16 13:55:19 +0100

    3rd iteration of ignoring terraform config files

---
+++ infra/main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+    features {}
+    subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+}
